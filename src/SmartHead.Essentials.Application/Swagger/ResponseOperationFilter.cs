43767fd2cca88e76cd1da013f12d40d6f7281e43
Asylgareev Rustam <rustam@smarthead.ru>
2020-03-27 17:19:13 +0300

    Add ResponseOperationFilter

---
+++ src/SmartHead.Essentials.Application/Swagger/ResponseOperationFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.OpenApi.Models;
+using SmartHead.Essentials.Application.Controller;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace SmartHead.Essentials.Application.Swagger
+{
+    [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+    public class ResponseOperationFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var isController = context.MethodInfo.ReflectedType.BaseType == typeof(FormattedApiControllerBase) ||
+                               context.MethodInfo.ReflectedType.BaseType.BaseType == typeof(FormattedApiControllerBase) ||
+                               context.MethodInfo.ReflectedType.BaseType.BaseType.BaseType == typeof(FormattedApiControllerBase);
+            if (!isController) return;
+
+            if (!operation.Responses.ContainsKey("500"))
+                operation.Responses.Add("500", new OpenApiResponse {Description = SwaggerResponseMessages.ServerError});
+
+            var hasAuthAttribute = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AuthorizeAttribute>()
+                .Any();
+
+            if (hasAuthAttribute && !operation.Responses.ContainsKey("401"))
+                operation.Responses.Add("401", new OpenApiResponse {Description = SwaggerResponseMessages.Unauthorized});
+
+            if (hasAuthAttribute && !operation.Responses.ContainsKey("403"))
+                operation.Responses.Add("403", new OpenApiResponse {Description = SwaggerResponseMessages.Forbidden});
+        }
+    }
+}
---
+++ src/SmartHead.Essentials.Application/Swagger/SwaggerResponseMessages.cs
@@ -0,0 +1,26 @@
+﻿namespace SmartHead.Essentials.Application.Swagger
+{
+    public class SwaggerResponseMessages
+    {
+        public const string Forbidden =
+            "\"Запрещено\". У клиента нет прав доступа к содержимому, поэтому сервер отказывается дать надлежащий ответ.";
+
+        public const string Unauthorized =
+            "\"Неавторизовано\". Для получения запрашиваемого ответа нужна аутентификация.";
+
+        public const string Ok =
+            "\"Успешно\". Запрос успешно обработан.";
+
+        public const string NoContent =
+            "\"Нет содержимого\". Нет содержимого для ответа на запрос.";
+
+        public const string Created =
+            "\"Создано\". Запрос успешно выполнен и в результате был создан ресурс.";
+
+        public const string BadRequest =
+            "\"Плохой запрос\". Этот ответ означает, что сервер не понимает запрос из-за неверного синтаксиса.";
+
+        public const string ServerError =
+            "\"Внутренняя ошибка сервера\". Сервер столкнулся с ситуацией, которую он не знает как обработать.";
+    }
+}
