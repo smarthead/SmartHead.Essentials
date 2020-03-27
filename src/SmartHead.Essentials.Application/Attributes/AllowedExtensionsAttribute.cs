e783f9d7fd9b39bb40add3534d1fe50806e8550d
Asylgareev Rustam <rustam@smarthead.ru>
2020-03-27 12:25:37 +0300

    Add HasValidFileNameAttribute and AllowedExtensionsAttribute

--- src/SmartHead.Essentials.Application/Attributes/AllowedExtensionsAttribute.cs
+++ src/SmartHead.Essentials.Application/Attributes/AllowedExtensionsAttribute.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 
 namespace SmartHead.Essentials.Application.Attributes
 {
-    class AllowedExtensionsAttribute
+    public class AllowedExtensionsAttribute : ValidationAttribute
     {
+        private readonly string[] _extensions;
+
+        public AllowedExtensionsAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            if (!(value is IFormFile file))
+                return new ValidationResult(typeof(IFormFile) + " is required.");
+
+            var extension = Path.GetExtension(file.FileName);
+
+            return !_extensions.Contains(extension.ToLower())
+                ? new ValidationResult(GetErrorMessage(extension))
+                : ValidationResult.Success;
+        }
+
+        public virtual string GetErrorMessage(string extension) => $"The {extension} extension is not allowed!";
     }
 }
---
+++ src/SmartHead.Essentials.Application/Attributes/HasValidFileNameAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.IO;
+using Microsoft.AspNetCore.Http;
+
+namespace SmartHead.Essentials.Application.Attributes
+{
+    public class HasValidFileNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(
+            object value, ValidationContext validationContext)
+        {
+            if (!(value is IFormFile file))
+                return new ValidationResult(typeof(IFormFile) + " is required.");
+
+            var isValid = !string.IsNullOrEmpty(file.FileName) &&
+                          file.FileName.IndexOfAny(Path.GetInvalidFileNameChars()) < 0;
+
+            return !isValid
+                ? new ValidationResult(GetErrorMessage())
+                : ValidationResult.Success;
+        }
+
+        public virtual string GetErrorMessage() =>
+            "The file name contains invalid characters! Rename the file and try again.";
+    }
+}
