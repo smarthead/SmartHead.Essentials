c074919e8f00cb1d42b83bd53638ab7a0e69032e
Rustam Asylgareev <rustamasylgareev@gmail.com>
2020-04-08 16:45:15 +0300

    Add Items total

--- src/SmartHead.Essentials.Application/Pagination/PagingModel.cs
+++ src/SmartHead.Essentials.Application/Pagination/PagingModel.cs
@@ -9,9 +9,11 @@ namespace SmartHead.Essentials.Application.Pagination
             Page = page;
             Total = (int) Math.Ceiling(count / (double) size);
             Size = size;
+            ItemsTotal = count;
         }
 
-        public int Page{ get; }
+        public int ItemsTotal { get; }
+        public int Page { get; }
         public int Total { get; }
         public int Size { get; }
         public bool HasPrevious => Page > 1;
