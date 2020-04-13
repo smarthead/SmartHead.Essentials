7daa1fdeac17a9cec65f36e70392229069cafed1
Rustam Asylgareev <rustamasylgareev@gmail.com>
2020-04-14 00:16:28 +0300

    Fix ResponseOperationFilter

--- src/SmartHead.Essentials.Application/Swagger/ResponseOperationFilter.cs
+++ src/SmartHead.Essentials.Application/Swagger/ResponseOperationFilter.cs
@@ -25,11 +25,16 @@ namespace SmartHead.Essentials.Application.Swagger
                 .OfType<AuthorizeAttribute>()
                 .Any();
 
-            if (hasAuthAttribute && !operation.Responses.ContainsKey("401"))
-                operation.Responses.Add("401", new OpenApiResponse {Description = SwaggerResponseMessages.Unauthorized});
+            var hasAllowAnonymousAttribute = context.MethodInfo.DeclaringType.GetCustomAttributes(true)
+                .Union(context.MethodInfo.GetCustomAttributes(true))
+                .OfType<AllowAnonymousAttribute>()
+                .Any();
 
-            if (hasAuthAttribute && !operation.Responses.ContainsKey("403"))
-                operation.Responses.Add("403", new OpenApiResponse {Description = SwaggerResponseMessages.Forbidden});
+            if (!hasAllowAnonymousAttribute && hasAuthAttribute && !operation.Responses.ContainsKey("401"))
+                operation.Responses.Add("401", new OpenApiResponse { Description = SwaggerResponseMessages.Unauthorized });
+
+            if (!hasAllowAnonymousAttribute && hasAuthAttribute && !operation.Responses.ContainsKey("403"))
+                operation.Responses.Add("403", new OpenApiResponse { Description = SwaggerResponseMessages.Forbidden });
         }
     }
 }
