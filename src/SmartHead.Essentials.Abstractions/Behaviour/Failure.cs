4dfe58e34c009c11c8c8968c5bee7a58cb5a2f6c
Артем Николаев <picikak03@gmail.com>
2020-05-25 14:00:37 +0300

    Доработка Result по части статических методов генерации, сокрытия конструктора и реализации класса ошибок вместо исключения.

---
+++ src/SmartHead.Essentials.Abstractions/Behaviour/Failure.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SmartHead.Essentials.Abstractions.Behaviour
+{
+    public class Failure
+    {
+        public Failure(params Failure[] failures)
+        {
+            if (!failures.Any())
+            {
+                throw new ArgumentException(nameof(failures));
+            }
+
+            Message = string.Join(Environment.NewLine, failures.Select(x => x.Message));
+            var data = new List<object>();
+
+            data.AddRange(failures.Select(x => x.Data));
+
+            Data = data.AsReadOnly();
+        }
+
+        public Failure(string message)
+        {
+            Message = message;
+            Data = new List<object> { message }.AsReadOnly();
+        }
+
+        public Failure(Exception exception)
+        {
+            Message = exception.Message;
+            Data = new List<object> { exception }.AsReadOnly();
+        }
+
+        public Failure(string message, IReadOnlyList<object> data)
+        {
+            Message = message;
+            Data = data;
+        }
+
+        public string Message { get; }
+
+        public IReadOnlyList<object> Data { get; protected set; }
+    }
+}
--- src/SmartHead.Essentials.Abstractions/Behaviour/Result.cs
+++ src/SmartHead.Essentials.Abstractions/Behaviour/Result.cs
@@ -2,33 +2,89 @@
 
 namespace SmartHead.Essentials.Abstractions.Behaviour
 {
-    public class Result<T>
+    public class Result
     {
-        public Result(Exception exception)
+        public static Result Success() => new Result(true);
+
+        public static Result Fail() => new Result(false);
+
+        public static Result Fail(Exception exception) => new Result(exception);
+
+        public static Result Fail(Failure failure) => new Result(failure);
+
+        public static Result Fail(Failure[] failures) => new Result(failures);
+
+        protected Result(Exception exception)
         {
-            Exception = exception;
+            ExplicitSuccess = false;
+            Failure = new Failure(exception);
         }
 
-        public Result(T payload)
+        protected Result(Failure failure)
         {
-            ExplicitSuccess = true;
+            ExplicitSuccess = false;
+            Failure = failure;
+        }
+
+        protected Result(Failure[] failures)
+        {
+            ExplicitSuccess = false;
+            Failure = new Failure(failures);
+        }
+
+        protected Result(bool success)
+        {
+            ExplicitSuccess = success;
+        }
+
+        public Failure Failure { get; protected set; }
+
+        public static implicit operator bool(Result r) => r.IsSuccess;
+
+        public bool IsSuccess => Failure == null && ExplicitSuccess;
+
+        public bool ExplicitSuccess { get; protected set; }
+    }
+
+    public class Result<T> : Result
+    {
+        public static Result<T> Success(T payload) => new Result<T>(payload);
+
+        public new static Result<T> Success() => new Result<T>(true);
+
+        public new static Result<T> Fail() => new Result<T>(false);
+
+        public new static Result Fail(Exception exception) => new Result<T>(exception);
+
+        public new static Result Fail(Failure failure) => new Result<T>(failure);
+
+        public new static Result Fail(Failure[] failures) => new Result<T>(failures);
+
+        protected Result(T payload) : base(true)
+        {
             Payload = payload;
         }
 
-        public Result(ResultType resultType)
+        protected Result(Exception exception) : base(exception)
         {
-            ExplicitSuccess = resultType == ResultType.Success;
             Payload = default;
         }
 
-        public Exception Exception { get; }
+        protected Result(Failure failure) : base(failure)
+        {
+            Payload = default;
+        }
+
+        protected Result(Failure[] failures): base(failures)
+        {
+            Payload = default;
+        }
+
+        protected Result(bool success) : base(success)
+        {
+            Payload = default;
+        }
 
         public T Payload { get; set; }
-
-        public bool IsSuccess => Exception == null && ExplicitSuccess;
-
-        public bool ExplicitSuccess { get; }
-
-        public static implicit operator bool(Result<T> r) => r.IsSuccess;
     }
 }
